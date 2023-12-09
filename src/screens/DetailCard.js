2b76503210dfa41c00eb1bf5c34fa634136291d1
Musadiq Jinabade <musaddiqjinabade@gmail.com>
2023-12-09 12:27:05 +0530

    first commit

--- src/screens/DetailCard.js
+++ src/screens/DetailCard.js
@@ -6,23 +6,11 @@ export default function DetailCard({route}) {
     const { item } = route.params;
     console.log("itemId", item)
     return (
-      <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView>
         <View
           style={{
             justifyContent: 'flex-start',
-            flexDirection:'row',
-            height: 190,
-            // borderWidth:0.3,
-            borderRadius: 20,
-            shadowColor: '#171717',
-            shadowOffset: {width: -2, height: 4},
-            shadowOpacity: 0.2,
-            shadowRadius: 3,
-            backgroundColor: '#B2E28D',
-          }}><View
-          style={{
-            justifyContent: 'flex-start',
-            flexDirection:'row',
+            flexDirection: 'row',
             height: 190,
             // borderWidth:0.3,
             borderRadius: 20,
@@ -32,28 +20,38 @@ export default function DetailCard({route}) {
             shadowRadius: 3,
             backgroundColor: '#B2E28D',
           }}>
-          <Text
+          <View
             style={{
-              fontWeight: 'bold',
               justifyContent: 'flex-start',
-              alignSelf: 'flex-start',
-              margin:10,
+              flexDirection: 'column',
+              height: 190,
             }}>
-            {item?.category}
-          </Text>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              justifyContent: 'flex-start',
-              alignSelf: 'flex-start',
-              margin:10,
-              fontSize:30
-            }}>
-            {item?.name}
-          </Text>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                justifyContent: 'flex-start',
+                alignSelf: 'flex-start',
+                marginHorizontal: 10,
+                margin: 5,
+              }}>
+              {item?.category}
+            </Text>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                justifyContent: 'flex-start',
+                alignSelf: 'flex-start',
+                marginHorizontal: 10,
+                margin: 5,
+                fontSize: 30,
+              }}>
+              {item?.name}
+            </Text>
           </View>
           <View>
-            <View style={{margin:20, borderRadius:20}}><Text></Text></View>
+            <View style={{margin: 20, borderRadius: 20}}>
+              <Text></Text>
+            </View>
           </View>
         </View>
       </SafeAreaView>
